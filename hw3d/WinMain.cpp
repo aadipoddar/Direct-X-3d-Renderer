6ee88ff1fcb698ceeba74c97cdef183c8032fe9b
Aadi Poddar <aadipoddarmail@gmail.com>
2021-09-04 11:49:12 +0530

    basic Window class

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,103 +1,27 @@
-#include <Windows.h>
-#include "WindowsMessageMap.h"
-#include <sstream>
+#include "Window.h"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	static WindowsMessageMap mm;
-	OutputDebugStringA(mm(msg, lParam, wParam).c_str());
 
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(69);
-		break;
-	case WM_KEYDOWN:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, L"Respects");
-		}
-		break;
-	case WM_KEYUP:
-		if (wParam == 'F')
-		{
-			SetWindowText(hWnd, L"Danger Field");
-		}
-		break;
-	case WM_CHAR:
-	{
-		static std::string title;
-		title.push_back((char)wParam);
-		SetWindowTextA(hWnd, title.c_str());
-	}
-	break;
-	case WM_LBUTTONDOWN:
-	{
-		const POINTS pt = MAKEPOINTS(lParam);
-		std::ostringstream oss;
-		oss << "(" << pt.x << "," << pt.y << ")";
-		SetWindowTextA(hWnd, oss.str().c_str());
-	}
-	break;
-	}
-
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
-
-int  CALLBACK WinMain(
+int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
-	LPSTR	  lpCmdLine,
-	int		  nCmdShow)
+	LPSTR     lpCmdLine,
+	int       nCmdShow )
 {
-	const auto pClassName = L"hw3dbutts";
+	Window wnd( 800,300,"Donkey Fart Box" );
+	Window wnd2( 200,650,"Donkey Fart Box2" );
 
-	// Register Window Class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-
-	RegisterClassEx(&wc);
-
-	// Create Window Instance
-	HWND hWnd = CreateWindowEx(
-		0, pClassName,
-		L"Happy Hard Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
-	);
-
-	// Show Window
-	ShowWindow(hWnd, SW_SHOW);
-
-	// Message Pump
 	MSG msg;
 	BOOL gResult;
-
-	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	while( (gResult = GetMessage( &msg,nullptr,0,0 )) > 0 )
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		TranslateMessage( &msg );
+		DispatchMessage( &msg );
 	}
 
-	if (gResult == -1)
+	if( gResult == -1 )
 	{
 		return -1;
 	}
 
-	else
-	{
-		return msg.wParam;
-	}
+	return msg.wParam;
 }

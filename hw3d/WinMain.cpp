56d5ecc001fdd7c704a8eeea5ce06fa672b58a96
Aadi Poddar <aadipoddarmail@gmail.com>
2021-09-04 12:17:01 +0530

    custom exception types

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -8,20 +8,22 @@ int CALLBACK WinMain(
 	int       nCmdShow )
 {
 	Window wnd( 800,300,"Donkey Fart Box" );
-	Window wnd2( 200,650,"Donkey Fart Box2" );
 
 	MSG msg;
 	BOOL gResult;
 	while( (gResult = GetMessage( &msg,nullptr,0,0 )) > 0 )
 	{
+		// TranslateMessage will post auxilliary WM_CHAR messages from key msgs
 		TranslateMessage( &msg );
 		DispatchMessage( &msg );
 	}
 
+	// check if GetMessage call itself borked
 	if( gResult == -1 )
 	{
 		return -1;
 	}
 
+	// wParam here is the value passed to PostQuitMessage
 	return msg.wParam;
 }

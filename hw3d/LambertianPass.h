7cfd00564155cdafbda9c50866c9ce32980ecd8f
Aadi Poddar <aadipoddarmail@gmail.com>
2021-09-13 01:22:41 +0530

    Slope-scaled Shadow Sandbox

--- hw3d/LambertianPass.h
+++ hw3d/LambertianPass.h
@@ -20,15 +20,14 @@ namespace Rgph
 		LambertianPass( Graphics& gfx,std::string name )
 			:
 			RenderQueuePass( std::move( name ) ),
-			pShadowCBuf{ std::make_shared<Bind::ShadowCameraCBuf>( gfx ) },
-			pShadowSampler{ std::make_shared<Bind::ShadowSampler>( gfx ) }
+			pShadowCBuf{ std::make_shared<Bind::ShadowCameraCBuf>( gfx ) }
 		{
 			using namespace Bind;
 			AddBind( pShadowCBuf );
-			AddBind( pShadowSampler );
 			RegisterSink( DirectBufferSink<RenderTarget>::Make( "renderTarget",renderTarget ) );
 			RegisterSink( DirectBufferSink<DepthStencil>::Make( "depthStencil",depthStencil ) );
 			AddBindSink<Bind::Bindable>( "shadowMap" );
+			AddBind( std::make_shared<Bind::ShadowSampler>( gfx ) );
 			RegisterSource( DirectBufferSource<RenderTarget>::Make( "renderTarget",renderTarget ) );
 			RegisterSource( DirectBufferSource<DepthStencil>::Make( "depthStencil",depthStencil ) );
 			AddBind( Stencil::Resolve( gfx,Stencil::Mode::Off ) );
@@ -49,7 +48,6 @@ namespace Rgph
 			RenderQueuePass::Execute( gfx );
 		}
 	private:
-		std::shared_ptr<Bind::ShadowSampler> pShadowSampler;
 		std::shared_ptr<Bind::ShadowCameraCBuf> pShadowCBuf;
 		const Camera* pMainCamera = nullptr;
 	};

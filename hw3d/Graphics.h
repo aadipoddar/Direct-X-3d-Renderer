3269dacd46116dc87c406d8adf3dff404453b400
Aadi Poddar <aadipoddarmail@gmail.com>
2021-09-12 17:18:55 +0530

    Test GUI (ImGui) / Camera

--- hw3d/Graphics.h
+++ hw3d/Graphics.h
@@ -56,12 +56,19 @@ public:
 	Graphics& operator=( const Graphics& ) = delete;
 	~Graphics() = default;
 	void EndFrame();
-	void ClearBuffer( float red,float green,float blue ) noexcept;
+	void BeginFrame( float red,float green,float blue ) noexcept;
 	void DrawIndexed( UINT count ) noexcept(!IS_DEBUG);
 	void SetProjection( DirectX::FXMMATRIX proj ) noexcept;
 	DirectX::XMMATRIX GetProjection() const noexcept;
+	void SetCamera( DirectX::FXMMATRIX cam ) noexcept;
+	DirectX::XMMATRIX GetCamera() const noexcept;
+	void EnableImgui() noexcept;
+	void DisableImgui() noexcept;
+	bool IsImguiEnabled() const noexcept;
 private:
 	DirectX::XMMATRIX projection;
+	DirectX::XMMATRIX camera;
+	bool imguiEnabled = true;
 #ifndef NDEBUG
 	DxgiInfoManager infoManager;
 #endif
